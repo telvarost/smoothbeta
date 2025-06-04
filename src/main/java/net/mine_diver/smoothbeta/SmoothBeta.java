9f3d9182992a416f697042b118f2744485f81b0c
Telvarost <132113523+telvarost@users.noreply.github.com>
2025-06-04 11:31:45 -0400

    Update to StationAPI 2.0.0-alpha.4 (#6)
    * Update to StationAPI 2.0.0-alpha.4
    
    * Fix GitHub workflow
    
    * Fix incorrect shadow variable

--- src/main/java/net/mine_diver/smoothbeta/SmoothBeta.java
+++ src/main/java/net/mine_diver/smoothbeta/SmoothBeta.java
@@ -3,14 +3,12 @@ package net.mine_diver.smoothbeta;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.mod.entrypoint.EventBusPolicy;
 import net.modificationstation.stationapi.api.util.Namespace;
-import net.modificationstation.stationapi.api.util.Null;
 import org.apache.logging.log4j.Logger;
 
 @Entrypoint(eventBus = @EventBusPolicy(registerStatic = false, registerInstance = false))
 public class SmoothBeta {
-    @Entrypoint.Namespace
-    public static final Namespace NAMESPACE = Null.get();
+    @SuppressWarnings("UnstableApiUsage")
+    public static final Namespace NAMESPACE = Namespace.resolve();
 
-    @Entrypoint.Logger
-    public static final Logger LOGGER = Null.get();
+    public static final Logger LOGGER = NAMESPACE.getLogger();
 }
